d30fbc4c539865c4efadeec67ecbf36eb60ae217
MitchLloyd <mitchelllloyd154@gmail.com>
2024-07-22 19:53:44 +0000

    setup nextjs project

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  plugins: ['react'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {
+    // Add your custom ESLint rules here
+  },
+};
