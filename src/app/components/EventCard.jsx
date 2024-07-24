9079abe740fab2237aade7df34ed776bf3429670
Marie C <152165776+mariecollingeada@users.noreply.github.com>
2024-07-24 10:45:33 +0000

    remove unnecessary console logs

--- src/app/components/EventCard.jsx
+++ src/app/components/EventCard.jsx
@@ -1,19 +1,40 @@
 export default function EventCard(props) {
+  const date = new Date(props.dates.start.dateTime);
+  const month   = date.getUTCMonth() + 1;
+  const day = date.getDate();
+  const venue = props._embedded.venues[0].name;
+  const city = props._embedded.venues[0].city.name;
+
+
   return (
-    <div key={props.key} className="event-card">
+    <div key={props.id} className="event-card">
       <div className="event-date">
-        {props.month}
-        <span>{props.day}</span>
+        {getMonthName(month)}
+        <span>{day}</span>
       </div>
       <ul className="event-details">
         <li className="event-name">{props.name}</li>
-        <li className="event-location">{props.location}</li>
-        <li className="event-venue">{props.venue}</li>
+        <li className="event-location">{city}</li>
+        <li className="event-venue">{venue}</li>
         {props.onPartnerSite && <li className="event-partner-site">On Partner Site</li>}
       </ul>
-      <a href={props.url} class="event-link">
+      <a href={props.url} className="event-link">
         Find tickets
       </a>
     </div>
   );
 }
+
+function getMonthName(monthNumber) {
+  const monthNames = [
+    'January', 'February', 'March', 'April', 'May', 'June', 
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+
+  if (monthNumber < 1 || monthNumber > 12) {
+    return 'Invalid month';
+  }
+
+  return monthNames[monthNumber - 1];
+}
+
--- src/app/components/EventList.jsx
+++ src/app/components/EventList.jsx
@@ -1,7 +1,5 @@
 import EventCard from './EventCard.jsx';
 
-console.log(process.env.TICKETMASTER_API_KEY + "API KEY");
-
 async function fetchEvents() {
   const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${process.env.TICKETMASTER_API_KEY}`);
   if (!response.ok) {
